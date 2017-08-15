415b442cf0931dd8c414acc469737a9f15c62dc8
Brian Schroeder <bts@gmail.com>
2017-08-15 08:57:52 -0400

    wip

--- Yesod/Auth/OAuth2.hs
+++ Yesod/Auth/OAuth2.hs
@@ -26,6 +26,7 @@ import Control.Applicative ((<$>))
 import Control.Exception.Lifted
 import Control.Monad.IO.Class
 import Control.Monad (unless)
+import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
 import Data.Monoid ((<>))
 import Data.Text (Text, pack)
@@ -35,17 +36,25 @@ import Data.Typeable
 import Network.HTTP.Conduit (Manager)
 import Network.OAuth.OAuth2
 import System.Random
+import URI.ByteString (URI, parseURI, queryPairs, serializeURIRef',
+                       strictURIParserOptions, uriQuery)
 import Yesod.Auth
 import Yesod.Core
 
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy.Char8 as C8L
+import qualified Data.Text as T
 
 -- | Provider name and Aeson parse error
 data YesodOAuth2Exception = InvalidProfileResponse Text BL.ByteString
     deriving (Show, Typeable)
 
 instance Exception YesodOAuth2Exception
+
+parseUrl :: ByteString -> Either Text URI
+parseUrl = first (T.pack . show)
+         . parseURI strictURIParserOptions
 
 oauth2Url :: Text -> AuthRoute
 oauth2Url name = PluginR name ["forward"]
@@ -57,7 +66,7 @@ oauth2Url name = PluginR name ["forward"]
 authOAuth2 :: YesodAuth m
            => Text   -- ^ Service name
            -> OAuth2 -- ^ Service details
-           -> (Manager -> AccessToken -> IO (Creds m))
+           -> (Manager -> OAuth2Token -> IO (Creds m))
            -- ^ This function defines how to take an @'AccessToken'@ and
            --   retrieve additional information about the user, to be
            --   set in the session as @'Creds'@. Usually this means a
@@ -76,26 +85,32 @@ authOAuth2Widget :: YesodAuth m
                  => WidgetT m IO ()
                  -> Text
                  -> OAuth2
-                 -> (Manager -> AccessToken -> IO (Creds m))
+                 -> (Manager -> OAuth2Token -> IO (Creds m))
                  -> AuthPlugin m
 authOAuth2Widget widget name oauth getCreds = AuthPlugin name dispatch login
 
   where
-    url = PluginR name ["callback"]
+    route = PluginR name ["callback"]
 
     withCallback csrfToken = do
         tm <- getRouteToParent
         render <- lift getUrlRender
-        return oauth
-            { oauthCallback = Just $ encodeUtf8 $ render $ tm url
-            , oauthOAuthorizeEndpoint = oauthOAuthorizeEndpoint oauth
-                `appendQuery` "state=" <> encodeUtf8 csrfToken
-            }
+        let parsedCbUrl = parseUrl $ encodeUtf8 $ render $ tm route
+        case parsedCbUrl of
+            Left err ->
+                permissionDenied $ "failed to parse callback URL: " <> err
+            Right cbUrl ->
+                let endpoint' = oauthOAuthorizeEndpoint oauth `addQueryPair`
+                                  ("state", encodeUtf8 csrfToken)
+                in return oauth { oauthCallback = Just cbUrl
+                                , oauthOAuthorizeEndpoint = endpoint'
+                                }
 
     dispatch "GET" ["forward"] = do
         csrfToken <- liftIO generateToken
         setSession tokenSessionKey csrfToken
-        authUrl <- bsToText . authorizationUrl <$> withCallback csrfToken
+        authUrl <- bsToText . serializeURIRef' . authorizationUrl <$>
+            withCallback csrfToken
         lift $ redirect authUrl
 
     dispatch "GET" ["callback"] = do
@@ -103,19 +118,19 @@ authOAuth2Widget widget name oauth getCreds = AuthPlugin name dispatch login
         oldToken <- lookupSession tokenSessionKey
         deleteSession tokenSessionKey
         unless (oldToken == Just csrfToken) $ permissionDenied "Invalid OAuth2 state token"
-        code <- requireGetParam "code"
+        code <- ExchangeToken <$> requireGetParam "code"
         oauth' <- withCallback csrfToken
         master <- lift getYesod
-        result <- liftIO $ fetchAccessToken (authHttpManager master) oauth' (encodeUtf8 code)
+        result <- liftIO $ fetchAccessToken (authHttpManager master) oauth' code
         case result of
             Left _ -> permissionDenied "Unable to retreive OAuth2 token"
             Right token -> do
                 creds <- liftIO $ getCreds (authHttpManager master) token
                 lift $ setCredsRedirect creds
           where
-              requireGetParam key = do
-                  m <- lookupGetParam key
-                  maybe (permissionDenied $ "'" <> key <> "' parameter not provided") return m
+            requireGetParam key = do
+                m <- lookupGetParam key
+                maybe (permissionDenied $ "'" <> key <> "' parameter not provided") return m
 
     dispatch _ _ = notFound
 
@@ -134,22 +149,24 @@ fromProfileURL :: FromJSON a
                => Text           -- ^ Plugin name
                -> URI            -- ^ Profile URI
                -> (a -> Creds m) -- ^ Conversion to Creds
-               -> Manager -> AccessToken -> IO (Creds m)
+               -> Manager
+               -> AccessToken
+               -> IO (Creds m)
 fromProfileURL name url toCreds manager token = do
     result <- authGetJSON manager token url
 
     case result of
         Right profile -> return $ toCreds profile
-        Left err -> throwIO $ InvalidProfileResponse name err
+        Left err -> throwIO $ InvalidProfileResponse name err -- (C8L.pack $ show err)
 
 bsToText :: ByteString -> Text
 bsToText = decodeUtf8With lenientDecode
 
-appendQuery :: ByteString -> ByteString -> ByteString
-appendQuery url query =
-    if '?' `C8.elem` url
-        then url <> "&" <> query
-        else url <> "?" <> query
+addQueryPair :: URI -> (ByteString, ByteString) -> URI
+addQueryPair uri pair = uri { uriQuery = query' }
+  where
+    query = uriQuery uri
+    query' = query { queryPairs = pair : queryPairs query }
 
 -- | A helper for providing an optional value to credsExtra
 --
